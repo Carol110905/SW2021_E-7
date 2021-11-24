c78f46a73b50ab040d703679d768c4fa5eb5ba27
JoaquinGA01 <otrebla.2817@gmail.com>
2021-11-24 09:32:06 -0600

    Proyect meaven

---
+++ Proyecto-Final/src/main/java/com/ProyectoAccesibilidad/Principal/App.java
@@ -0,0 +1,13 @@
+package com.ProyectoAccesibilidad.Principal;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
