0319bd0701d7cdc087c33457dcd1719b6e4639e1
JoaquinGA01 <otrebla.2817@gmail.com>
2021-11-26 15:49:42 -0600

    Diagramas

--- Proyecto-Final/src/proyectofinal.sql
+++ Proyecto-Final/src/proyectofinal.sql
@@ -1,31 +1,5 @@
--- phpMyAdmin SQL Dump
--- version 5.1.1
--- https://www.phpmyadmin.net/
---
--- Servidor: 127.0.0.1
--- Tiempo de generación: 24-11-2021 a las 19:29:44
--- Versión del servidor: 10.4.20-MariaDB
--- Versión de PHP: 8.0.9
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Base de datos: `proyectofinal`
---
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `examen`
---
 
 CREATE TABLE `examen` (
   `idExamen` int(255) NOT NULL,
@@ -35,7 +9,9 @@ CREATE TABLE `examen` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 
-Create tabla 'RelacionUE' (
+
+
+CREATE TABLE 'RelacionUE' (
   'idExamen' int(255) NOT NULL,
   'idUsuarios' int(255) NOT NULL,
 
